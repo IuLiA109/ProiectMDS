cff55a8def527cfaf9d1d7508617ada89703dd81
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2024-06-16 16:16:23 +0300

    Reapply "AI First stepts"
    This reverts commit d47ccacf258c89221500ffd0ff93b74e7564e705.

--- MarioWithAI/game.py
+++ MarioWithAI/game.py
@@ -10,6 +10,7 @@ from Levels.level1 import Level1
 from hud import HUD
 from menu import Menu
 from sound import Sound
+from ai import AIPlayer
 
 class GameController:
     def __init__(self):
@@ -101,6 +102,9 @@ class GameController:
         self.sound = Sound()
         self.sound.play_music('soundtrack')  # Play background music
 
+        self.ai = AIPlayer(self)
+        self.ai_enabled = True  # Set this to False to disable AI control
+
     def restartGame(self):
         self.currentLevel.init_Level()
 
@@ -189,6 +193,8 @@ class GameController:
             self.menu.update()
 
         if self.gameStateManager.gameState == "Level 1":
+            if self.ai_enabled:
+                self.player.apply_ai_actions()
             self.Level1.updateLevel()
 
         self.hud.updateHUD()
